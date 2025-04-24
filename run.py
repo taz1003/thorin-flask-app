1439195407de856d8d35a262f2c10314141946b1
taz1003 <li9vyrit@students.codeinstitute.net>
2025-04-24 18:11:00 +0200

    update run.py

--- run.py
+++ run.py
@@ -11,12 +11,23 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
     data = []
-    with open('data/company.json', 'r') as json_data:
+    with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template('about.html', page_title='About', company=data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return "<h1>" + member['name'] + "</h1>"
 
 
 @app.route('/contact')
